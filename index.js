3636d5a1cddc3927d5ccf6cc11ec40ca122f7fab
Loaye <jr.carstens00@gmail.com>
2017-07-21 15:53:27 -0700

    fixes index.js to use proper file paths

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ const fs = require('fs');
 
 var read = require(`${__dirname}/lib/read-file.js`);
 var write = require(`${__dirname}/lib/write-file.js`);
-var transform = require(`${__dirname}/lib/transform.js`);
