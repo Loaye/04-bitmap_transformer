f43ebd01bda65605307465bed27e660ee02fcb07
JohnScarrow <johna@scarrows.com>
2017-07-21 18:36:55 -0700

    working in current state

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 const fs = require('fs');
 
-var read = require(`${__dirname}/lib/read-file.js`);
-var write = require(`${__dirname}/lib/write-file.js`);
+// const Bitmap = require(`${__dirname}/model/bitmap-constructor.js`);
+const read = require(`${__dirname}/lib/read-file.js`);
+const write = require(`${__dirname}/lib/write-file.js`);
+var bitmapImage = read(`${__dirname}/assets/palette-bitmap.bmp`, (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+console.log(bitmapImage, 'final test');
--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 //Gets bitmap data and put into construtor
-const Bitmap = module.exports = function(data) {
+module.exports = function(data) {
   this.data = data;
   this.id = data.toString('utf-8', 0, 2);
   this.size = data.readUInt32LE(2);
@@ -11,30 +11,29 @@ const Bitmap = module.exports = function(data) {
   this.width = data.readUInt32LE(22);
   this.pixelOffSet = data.readUInt32LE(10);
   this.colorArray = data.readUInt32LE(54, this.pixelOffSet);
+  //blackout
 };
-
-//blackout
-Bitmap.prototype.blackOut = function() {
+module.exports.prototype.blackOut = function() {
   this.colorArray.fill(0);
-};
+  };
 
 //whiteout
-Bitmap.prototype.blackOut = function() {
+module.exports.prototype.whiteOut = function() {
   this.colorArray.fill(f);
 };
 
 //greyout
-Bitmap.prototype.blackOut = function() {
+module.exports.prototype.grayOut = function() {
   this.colorArray.fill(c);
 };
 
 //reverse
-Bitmap.prototype.reversed = function() {
+module.exports.prototype.reversed = function() {
   this.pixelOffSet.reverse();
 };
 
 //random
-Bitmap.prototype.random = function(){
+module.exports.prototype.random = function(){
   for(var i = 0; i< this.colorArray.length; i+=4){
     this.colorArray[i] = Math.floor(Math.random() *255) +1;
   };
